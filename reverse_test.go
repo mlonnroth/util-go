c85cea087c8671591130d61752cce9a23261d959
Magnus <magnus.lonnroth@gmail.com>
2018-02-01 09:23:48 +0100

    Initial version

---
+++ reverse_test.go
@@ -0,0 +1,21 @@
+package stringutil
+
+import "testing"
+import "fmt"
+
+func TestReverse(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
+	fmt.Printf("Test 1: ")
+	for _, c := range cases {
+		got := Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
